2c9605fa4a25ce894f2ab265bea3cb8391a1d94b
Jackson Otsig <92471448+JackOtsig@users.noreply.github.com>
2022-01-15 15:26:29 -0800

    Update dawg.py

--- dawg.py
+++ dawg.py
@@ -19,8 +19,6 @@ timer = False
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-#id n2dLeMAmS7u-nna6svhyAg
 @bot.command()
 async def verify(ctx, cred, username):
     member_role = discord.utils.find(lambda r: r.name == 'Member', ctx.message.guild.roles)
@@ -35,7 +33,7 @@ async def verify(ctx, cred, username):
         officer_role,
         rh_role,
     ]
-    if ctx.channel.id == 931293831582855189:
+    if ctx.channel.id == :
         member = ctx.message.author
         roles = member.roles
         clear = True
@@ -45,7 +43,7 @@ async def verify(ctx, cred, username):
                     clear = False
         if clear:
             if cred == 'guild':
-                response = requests.get("https://gameinfo.albiononline.com/api/gameinfo/guilds/n2dLeMAmS7u-nna6svhyAg/members")
+                response = requests.get("https://gameinfo.albiononline.com/api/gameinfo/guilds//members")
                 match = False
                 for user in response.json():
                     if username.lower() == user['Name'].lower():
@@ -57,7 +55,7 @@ async def verify(ctx, cred, username):
                 else:
                     await ctx.send("Sorry, couldn't verify!")
             elif cred == 'alliance':
-                response = requests.get("https://gameinfo.albiononline.com/api/gameinfo/alliances/Yzq8-LgPTIm-9SMVdyAN8w")
+                response = requests.get("https://gameinfo.albiononline.com/api/gameinfo/alliances/")
                 guilds = response.json()['Guilds']
                 alliance_member_list = []
                 id_list = []
@@ -83,74 +81,6 @@ async def verify(ctx, cred, username):
     else:
         await ctx.send('wrong channel!')
 
-# def get_sent():
-#     f = open('sent.json','r')
-#     sent_list = json.load(f)
-#     f.close()
-#     return sent_list
-
-# def save_sent(id):
-#     f = open('sent.json', 'w')
-#     old_list = get_sent()
-#     old_list.append(id)
-#     new_list_json = json.dumps(old_list)
-#     f.write(new_list_json)
-#     return
-
-# async def kills():
-#     global kills_list
-#     print('refreshing kills')
-#     response = requests.get('https://gameinfo.albiononline.com/api/gameinfo/events?limit=40&offset=0&guildId=n2dLeMAmS7u-nna6svhyAg')
-#     print('response gotten')
-#     for kill in response.json():
-#         event_id = kill['EventId']
-#         killer = {
-#             'name' : kill['Killer']['Name'], 
-#             'gear' : kill['Killer']['Equipment'], 
-#             'guild' : [kill['Killer']['GuildName'], kill['Killer']['GuildId']],
-#             'alliance' : [kill['Killer']['AllianceName'], kill['Killer']['AllianceId']]
-#         }
-#         victim = {
-#             'name' : kill['Victim']['Name'], 
-#             'gear' : kill['Victim']['Equipment'], 
-#             'guild' : [kill['Victim']['GuildName'], kill['Victim']['GuildId']],
-#             'alliance' : [kill['Victim']['AllianceName'], kill['Victim']['AllianceId']]
-#         }
-#         kills_list.append({'id' : event_id, 'killer' : killer, 'victim' : victim, 'recap' : killer['name']+' killed '+victim['name']})
-#         while len(kills_list) > 5:
-#             kills_list.pop(0)
-#     print('kills updated')
-#     return kills_list
-
-# async def kills_not():
-#     print('starting notification')
-#     channel = bot.get_channel(927270590140792884)
-#     print('got channel')
-#     printed = get_sent()
-#     print('sent list gotten')
-#     match = False
-#     print('match set to false')
-#     kills_list = await kills()
-#     print('kills awaited')
-#     print('got kills')
-#     for kill in kills_list:
-#         if not printed:
-#             await channel.send(kill['recap'])
-#             save_sent(kill['id'])
-#         else:
-#             for log in printed:
-#                 if kill['id'] == log:
-#                     match = True
-#             if not match:
-#                 await channel.send(kill['recap'])
-#                 save_sent(kill['id'])
-#     return printed
-
-# @tasks.loop(seconds=15)
-# async def kbl():
-#     await kills_not()
-#     print('looped')
-# kbl.start()
 
 if __name__ == "__main__":
     bot.run(TOKEN)
